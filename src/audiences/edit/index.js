d06a71340040bcd3867e31e3f8366aec21b42532
Ryan McCue <me@ryanmccue.info>
2020-04-03 18:45:46 +0100

    Remove unused import

--- src/audiences/edit/index.js
+++ src/audiences/edit/index.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import AudienceEditor from './components/audience-editor';
