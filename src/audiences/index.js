7da7bbc1aad1320dcb0d8eaddc2bf3d1994a12d8
Robert O'Rourke <robert@o-rourke.org>
2020-12-11 12:43:41 +0000

    Proper version of ESLint running now!

--- src/audiences/index.js
+++ src/audiences/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Manager, Select } from './ui';
+
 // Import the data store file directly.
 import './ui/data';
 
@@ -19,6 +20,6 @@ if ( AudienceManager ) {
 	// Mount audience react app.
 	ReactDOM.render(
 		<Manager />,
-		AudienceManager,
+		AudienceManager
 	);
 }
