adf2a810efe3e9bd886fbdaa5b53e05822ff8c48
CircleCI <rob+platform-ui-build@hmn.md>
2021-02-17 15:15:23 +0000

    Build changes from 171d3bdae1e4f48ba76bc6787f2f5eff236b0ca6
    https://circleci.com/gh/humanmade/aws-analytics/75

---
+++ inc/experiments/titles/namespace.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Title AB testing feature.
+ *
+ * @package aws-analytics
+ */
+
+namespace Altis\Analytics\Experiments\Titles;
+
+use Altis\Analytics;
+use Altis\Analytics\Experiments;
+use Altis\Analytics\Utils;
+
+/**
+ * Bootstrap Title AB Tests Feature.
+ *
+ * @return void
+ */
+function setup() {
+	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\admin_scripts' );
+	add_action( 'init', __NAMESPACE__ . '\\init' );
+	add_action( 'init', __NAMESPACE__ . '\\register_default_post_type_support', 9 );
+}
+
+/**
+ * Load Block Editor sidebar plugin.
+ *
+ * @param string $hook Page hook suffix.
+ */
+function admin_scripts( string $hook ) {
+	if ( ! in_array( $hook, [ 'post.php', 'post-new.php' ], true ) ) {
+		return;
+	}
+
+	// Get supported post types.
+	$supported_post_types = get_post_types_by_support( 'altis.experiments.titles' );
+
+	if ( ! in_array( get_current_screen()->post_type, $supported_post_types, true ) ) {
+		return;
+	}
+
+	wp_enqueue_script(
+		'altis-experiments-features-titles',
+		Utils\get_asset_url( 'titles.js' ),
+		[
+			'wp-plugins',
+			'wp-blocks',
+			'wp-i18n',
+			'wp-editor',
+			'wp-components',
+			'wp-core-data',
+			'wp-edit-post',
+			'moment',
+		],
+		null
+	);
+
+	wp_add_inline_script(
+		'altis-experiments-features-titles',
+		sprintf(
+			'window.Altis = window.Altis || {};' .
+			'window.Altis.Analytics = window.Altis.Analytics || {};' .
+			'window.Altis.Analytics.Experiments = window.Altis.Analytics.Experiments || {};' .
+			'window.Altis.Analytics.Experiments.BuildURL = %s;',
+			wp_json_encode( plugins_url( 'build', Analytics\ROOT_FILE ) )
+		),
+		'before'
+	);
+}
+
+/**
+ * Sets up the default post type support for title A/B tests.
+ *
+ * @uses apply_filters( 'altis.experiments.features.titles.post_types', [] )
+ */
+function register_default_post_type_support() {
+	/**
+	 * Filters the default post types supported by experiments.
+	 *
+	 * @param array $post_types Supported post types for title AB tests.
+	 */
+	$post_types = apply_filters( 'altis.experiments.features.titles.post_types', [
+		'post',
+		'page',
+	] );
+
+	foreach ( $post_types as $post_type ) {
+		add_post_type_support( $post_type, 'altis.experiments.titles' );
+	}
+}
+
+/**
+ * Replace title output with the AB Test markup equivalent.
+ *
+ * @param string $title The current title text.
+ * @param integer $post_id The post ID.
+ * @return string
+ */
+function add_title_ab_test_to_title( string $title, int $post_id ) : string {
+	return Experiments\output_ab_test_html_for_post( 'titles', $post_id, $title );
+}
+
+/**
+ * Set up the post meta for our titles and create the tests.
+ */
+function init() {
+	if ( ! is_admin() ) {
+		add_filter( 'the_title', __NAMESPACE__ . '\\add_title_ab_test_to_title', 10, 2 );
+	}
+
+	Experiments\register_post_ab_test(
+		'titles',
+		[
+			'label' => __( 'Titles', 'altis-experiments' ),
+			'goal' => 'click',
+			'closest' => 'a',
+			// Exclude all events from the target post page.
+			'query_filter' => function ( $test_id, $post_id ) : array {
+				$url = get_the_permalink( $post_id );
+				return [
+					'must_not' => [
+						[ 'prefix' => [ 'attributes.url.keyword' => $url ] ],
+					],
+				];
+			},
+			// Update the actual post title.
+			'winner_callback' => function ( int $post_id, string $title ) {
+				wp_update_post( [
+					'ID' => $post_id,
+					'post_title' => $title,
+				] );
+			},
+			'post_types' => get_post_types_by_support( 'altis.experiments.titles' ),
+		]
+	);
+}
---
+++ src/audiences/ui/components/modal.js
@@ -0,0 +1,67 @@
+import React, { Component, Fragment } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+
+const { __ } = wp.i18n;
+
+const StyledModal = styled.div`
+	.media-frame-content {
+		bottom: 0;
+	}
+`;
+
+/**
+ * Audience Editor Modal.
+ */
+class Modal extends Component {
+	constructor( props ) {
+		super( props );
+		this.modalRoot = document.getElementById( 'altis-analytics-audience-modal' );
+		this.el = document.createElement( 'div' );
+	}
+
+	componentDidMount() {
+		this.modalRoot.appendChild( this.el );
+	}
+
+	componentWillUnmount() {
+		this.modalRoot.removeChild( this.el );
+	}
+
+	render() {
+		const {
+			children,
+			className,
+			onClose,
+			title,
+		} = this.props;
+
+		return createPortal(
+			<Fragment>
+				<StyledModal aria-labelledby="media-frame-title" className={ `media-modal wp-core-ui ${ className }` } role="dialog" tabIndex="0">
+					<button className="media-modal-close" type="button" onClick={ onClose }>
+						<span className="media-modal-icon">
+							<span className="screen-reader-text">{ __( 'Close dialog' ) }</span>
+						</span>
+					</button>
+					{ title && (
+						<div className="media-frame-title" id="media-frame-title">
+							<h1>{ title }</h1>
+						</div>
+					) }
+					<div className="media-modal-content" role="document">
+						<div className="media-frame wp-core-ui hide-menu hide-router">
+							<div className="media-frame-content">
+								{ children }
+							</div>
+						</div>
+					</div>
+				</StyledModal>
+				<div className="media-modal-backdrop"></div>
+			</Fragment>,
+			this.el
+		);
+	}
+}
+
+export default Modal;
