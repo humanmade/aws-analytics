df1d12546f2c0e3d2184a657180870de2800dd82
Chris Reynolds <chris@jazzsequence.com>
2021-03-05 13:51:20 -0700

    remove default columns from the post list

--- inc/dashboard/namespace.php
+++ inc/dashboard/namespace.php
@@ -7,4 +7,18 @@
 
 namespace Altis\Analytics\Dashboard;
 
-function setup() {}
+function setup() {
+	add_filter( 'manage_posts_columns', __NAMESPACE__ . '\\remove_default_columns', 10, 2 );
+}
+
+function remove_default_columns( $columns, $post_type ) {
+	if ( $post_type !== 'xb' ) {
+		return $columns;
+	}
+
+	unset( $columns['cb'] );
+	unset( $columns['title'] );
+	unset( $columns['altis_publication_checklist_status'] );
+
+	return $columns;
+}
