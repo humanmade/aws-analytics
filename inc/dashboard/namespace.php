25e3edd2ede377bc1b65bbe03ed458f820786cdf
Chris Reynolds <chris@jazzsequence.com>
2021-03-05 13:52:48 -0700

    remove the months dropdown filter

--- inc/dashboard/namespace.php
+++ inc/dashboard/namespace.php
@@ -11,6 +11,7 @@ function setup() {
 	add_filter( 'manage_posts_columns', __NAMESPACE__ . '\\remove_default_columns', 10, 2 );
 	add_filter( 'bulk_actions-edit-xb', '__return_empty_array' );
 	add_filter( 'views_edit-xb', '__return_null' );
+	add_filter( 'months_dropdown_results', __NAMESPACE__ . '\\remove_date_filter', 10, 2 );
 }
 
 function remove_default_columns( $columns, $post_type ) {
@@ -23,4 +24,12 @@ function remove_default_columns( $columns, $post_type ) {
 	unset( $columns['altis_publication_checklist_status'] );
 
 	return $columns;
+}
+
+function remove_date_filter( $months, $post_type ) {
+	if ( 'xb' === $post_type ) {
+		return [];
+	}
+
+	return $months;
 }
