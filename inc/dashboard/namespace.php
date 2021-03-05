ee7ec098ee94f472383ea8a731a97062ce7539f4
Chris Reynolds <chris@jazzsequence.com>
2021-03-05 13:52:25 -0700

    remove the bulk actions and views elements from the post list

--- inc/dashboard/namespace.php
+++ inc/dashboard/namespace.php
@@ -9,6 +9,8 @@ namespace Altis\Analytics\Dashboard;
 
 function setup() {
 	add_filter( 'manage_posts_columns', __NAMESPACE__ . '\\remove_default_columns', 10, 2 );
+	add_filter( 'bulk_actions-edit-xb', '__return_empty_array' );
+	add_filter( 'views_edit-xb', '__return_null' );
 }
 
 function remove_default_columns( $columns, $post_type ) {
