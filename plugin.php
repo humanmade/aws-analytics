8a66e7ce73b25d4d2076bb11fb1187660ac44248
Robert O'Rourke <robert@o-rourke.org>
2021-08-23 15:46:45 +0100

    Release 3.0.4

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Altis Analytics
  * Description: Analytics layer for Altis powered by AWS Pinpoint.
- * Version: 3.0.3
+ * Version: 3.0.4
  * Author: Human Made Limited
  * Author URI: https://humanmade.com/
  *
