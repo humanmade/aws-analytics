7780bbad1f4a08597e4112b7f20c8c10e9c0a189
CircleCI <rob+platform-ui-build@hmn.md>
2021-09-21 13:41:55 +0000

    Build changes from 2fc5fe297f8caa8743a8777346353d3ae286f031
    https://circleci.com/gh/humanmade/aws-analytics/125

---
+++ plugin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: Altis Analytics
+ * Description: Analytics layer for Altis powered by AWS Pinpoint.
+ * Version: 4.0.9
+ * Author: Human Made Limited
+ * Author URI: https://humanmade.com/
+ *
+ * @package aws-analytics
+ */
+
+namespace Altis\Analytics;
+
+const ROOT_DIR = __DIR__;
+const ROOT_FILE = __FILE__;
+
+// Check if this is installed as a self contained built version.
+if ( file_exists( ROOT_DIR . '/vendor/autoload.php' ) ) {
+	require_once ROOT_DIR . '/vendor/autoload.php';
+}
+
+require_once __DIR__ . '/inc/namespace.php';
+require_once __DIR__ . '/inc/audiences/namespace.php';
+require_once __DIR__ . '/inc/audiences/rest_api/class-posts-controller.php';
+require_once __DIR__ . '/inc/audiences/rest_api/namespace.php';
+require_once __DIR__ . '/inc/blocks/namespace.php';
+require_once __DIR__ . '/inc/dashboard/namespace.php';
+require_once __DIR__ . '/inc/blocks/rest_api/namespace.php';
+require_once __DIR__ . '/inc/blocks/rest_api/class-posts-controller.php';
+require_once __DIR__ . '/inc/experiments/namespace.php';
+require_once __DIR__ . '/inc/experiments/titles/namespace.php';
+require_once __DIR__ . '/inc/export/class-endpoint.php';
+require_once __DIR__ . '/inc/export/namespace.php';
+require_once __DIR__ . '/inc/preview/namespace.php';
+require_once __DIR__ . '/inc/utils/namespace.php';
+
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\setup' );
---
+++ src/utils/polyfills.js
@@ -0,0 +1,43 @@
+/**
+ * Polyfills CustomEvent.
+ */
+( function () {
+	if ( typeof window.CustomEvent === 'function' ) {
+		return false;
+	}
+
+	/**
+	 * Creates a custom event handler.
+	 *
+	 * @param {string} event The event name.
+	 * @param {object} params The event options.
+	 * @returns {Event} The new custom event.
+	 */
+	function CustomEvent( event, params ) {
+		params = params || {
+			bubbles: false,
+			cancelable: false,
+			detail: null,
+		};
+		let evt = document.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	}
+
+	window.CustomEvent = CustomEvent;
+} )();
+
+window.console = window.console || {
+	/**
+	 * Log placeholder function.
+	 */
+	log: function () { },
+	/**
+	 * Warn placeholder function.
+	 */
+	warn: function () { },
+	/**
+	 * Error placeholder function.
+	 */
+	error: function () { },
+};
