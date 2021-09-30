cf149f626ee61944f52f7a739d9d948e3ed284c7
CircleCI <rob+platform-ui-build@hmn.md>
2021-09-30 16:23:31 +0000

    Build changes from 09f3bc6079ff309b69c890b305364bf0fddf5a49
    https://circleci.com/gh/humanmade/aws-analytics/133

---
+++ inc/blocks/personalization/index.js
@@ -0,0 +1,45 @@
+
+import shimBlockData from '../shim/block.json';
+import blockData from './block.json';
+
+import Status from './components/status';
+import edit from './edit';
+import save from './save';
+import transforms from './transforms';
+
+const { registerBlockType } = wp.blocks;
+const { addFilter } = wp.hooks;
+const { __ } = wp.i18n;
+
+const settings = {
+	title: __( 'Personalized Content', 'altis-analytics' ),
+	description: __( 'Deliver personalized content to different audiences', 'altis-analytics' ),
+	keywords: [
+		__( 'experience', 'altis-analytics' ),
+		__( 'personalize', 'altis-analytics' ),
+		__( 'conditional', 'altis-analytics' ),
+		__( 'audience', 'altis-analytics' ),
+		__( 'analytics', 'altis-analytics' ),
+	],
+	edit,
+	save,
+	transforms,
+	...blockData.settings,
+};
+
+// Register block.
+registerBlockType( blockData.name, settings );
+
+// Register shim.
+registerBlockType( shimBlockData.name, {
+	title: 'shim',
+	...shimBlockData.settings,
+} );
+
+// Enhance publication checklist if available.
+addFilter( 'altis-publishing-workflow.item.xbs-valid-conversions', 'altis/xbs', () => {
+	return Status;
+} );
+addFilter( 'altis-publishing-workflow.item.xbs-valid-fallback', 'altis/xbs', () => {
+	return Status;
+} );
---
+++ src/blocks/ui/components/variants.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { compactMetric, formatNumber } from '../../../utils';
+import { defaultVariantAnalytics } from '../../data/shapes';
+
+import Lift from './lift';
+
+const { decodeEntities } = wp.htmlEntities;
+const { __ } = wp.i18n;
+
+const VariantsList = styled.div`
+	padding-bottom: 30px;
+
+	& h2 {
+		color: #4767df;
+	}
+
+	.altis-analytics-block-variant {
+		margin: 30px 0 40px;
+
+		&--fallback {
+			margin-bottom: 20px;
+			border-bottom: 1px solid rgba( 0, 0, 0, .2 );
+			padding-bottom: 20px;
+		}
+	}
+
+	ul {
+		list-style: none;
+		display: flex;
+		flex-wrap: wrap;
+		margin: 10px 0 0;
+		padding: 0;
+	}
+
+	li {
+		margin-right: 40px;
+		margin-top: 10px;
+		display: flex;
+		flex-direction: column-reverse;
+	}
+
+	h3, p {
+		margin: 0;
+	}
+
+	h3 {
+		margin-right: 40px;
+	}
+
+	li .description {
+		text-transform: uppercase;
+	}
+
+	.altis-analytics-block-variant__metric {
+		font-size: 24px;
+		font-weight: bold;
+		margin-bottom: 5px;
+	}
+
+	.audience-estimate svg {
+		max-height: 30px;
+	}
+
+	.blue {
+		color: #4767df;
+	}
+`;
+
+/**
+ * Show experience block variant data.
+ *
+ * @param {object} props The component props.
+ * @returns {React.ReactNode} The variants display component.
+ */
+function Variants( props ) {
+	const {
+		analytics,
+		variants,
+	} = props;
+
+	if ( ! variants ) {
+		return null;
+	}
+
+	const fallback = ( variants && variants.find( variant => variant.fallback ) ) || {};
+	const fallbackData = analytics ? ( analytics.audiences.find( audience => audience.id === 0 ) || defaultVariantAnalytics ) : null;
+
+	return (
+		<VariantsList>
+			<h2>{ __( 'Block Variants', 'altis-analytics' ) }</h2>
+			{ variants.map( variant => {
+				const data = ( analytics && analytics.audiences.find( audience => audience.id === ( variant.audience && variant.audience.id ) || 0 ) ) || defaultVariantAnalytics;
+				return (
+					<div className={ `altis-analytics-block-variant ${ variant.fallback ? 'altis-analytics-block-variant--fallback' : '' }` }>
+						<div className="altis-analytics-block-variant__header">
+							<h3>{ variant.fallback ? __( 'Fallback', 'altis-analytics' ) : decodeEntities( variant.audience.title ) }</h3>
+							{ variant.goal && ( <p className="description">{ __( 'Goal', 'altis-analytics' ) }: <strong>{ window.Altis.Analytics.Experiments.Goals[ variant.goal ].label }</strong></p> ) }
+						</div>
+						<ul>
+							<li>
+								<p className="description">{ __( 'Total views', 'altis-analytics' ) }</p>
+								<div className="altis-analytics-block-variant__metric">{ data ? formatNumber( data.views ) : '…' }</div>
+							</li>
+							<li>
+								<p className="description">{ __( 'Unique views', 'altis-analytics' ) }</p>
+								<div className="altis-analytics-block-variant__metric">{ data ? formatNumber( data.unique.views ) : '…' }</div>
+							</li>
+							{ variant.goal && (
+								<>
+									<li>
+										<p className="description">{ __( 'Conversion rate', 'altis-analytics' ) }</p>
+										<div className="altis-analytics-block-variant__metric">{ data ? compactMetric( ( data.unique.conversions / data.unique.views ) * 100 ) : '…' }</div>
+									</li>
+									{ ! variant.fallback && fallback.goal && fallbackData && (
+										<li>
+											<p className="description">{ __( 'Lift vs. fallback', 'altis-analytics' ) }</p>
+											<Lift
+												className="altis-analytics-block-variant__metric"
+												current={ data.unique.conversions / data.unique.views }
+												previous={ fallbackData.unique.conversions / fallbackData.unique.views }
+											/>
+										</li>
+									) }
+								</>
+							) }
+							{ ! variant.fallback && (
+								<li>
+									<p className="description">{ __( 'Audience coverage', 'altis-analytics' ) }</p>
+									<div className="altis-analytics-block-variant__metric blue">{ ( analytics && data ) ? compactMetric( ( data.unique.views / analytics.unique.views ) * 100 ) : '…' }</div>
+								</li>
+							) }
+						</ul>
+					</div>
+				);
+			} ) }
+		</VariantsList>
+	);
+}
+
+Variants.defaultProps = {
+	analytics: null,
+	variants: [],
+};
+
+export default Variants;
