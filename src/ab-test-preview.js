6b1a1942718e123994bc08e809e993edb583809e
Robin Devitt <robin@humanmade.com>
2021-12-13 09:59:37 +0200

    remove unused data attributes that are no longer necessary.

--- src/ab-test-preview.js
+++ src/ab-test-preview.js
@@ -30,7 +30,7 @@ document.querySelectorAll( '.ab-test-xb-preview' ).forEach( xb => {
 			const variant = templates[i].content.cloneNode( true );
 
 			// remove the active class from any tab
-			xb.querySelectorAll( '.ab-test-xb-preview__tabs[data-client-tabs="' + xb.dataset.clientId + '"] .ab-test-xb-preview__tab' ).forEach( el => {
+			xb.querySelectorAll( '.ab-test-xb-preview__tabs .ab-test-xb-preview__tab' ).forEach( el => {
 				el.classList.remove( 'active' );
 			} );
 
